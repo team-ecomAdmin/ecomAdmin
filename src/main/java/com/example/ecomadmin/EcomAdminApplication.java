71e6bf20047c41a5e264d3e34c1103ed7ff9fcb0
이희수 <leehsdd33@gmail.com>
2025-03-24 20:06:33 +0900

    first commit

---
+++ src/main/java/com/example/ecomadmin/EcomAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecomadmin/EcomAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
