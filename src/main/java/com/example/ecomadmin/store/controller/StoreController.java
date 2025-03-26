b7cd32d0ee72225f6a910e9ea3bf8bb16adde195
ansdudn2 <ansdudn2@naver.com>
2025-03-26 17:58:15 +0900

    fix controller add final

--- src/main/java/com/example/ecomadmin/store/controller/StoreController.java
+++ src/main/java/com/example/ecomadmin/store/controller/StoreController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class StoreController {
 
-    private StoreService storeService;
+    private final StoreService storeService;
 
     @PutMapping("/{storeId}")
     public StoreEntity updateStore(
